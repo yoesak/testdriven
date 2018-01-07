029cfeebfd6f591061900a5d82a3ce894fac0564
Yusak Setiawan <zivlab@Yusaks-MacBook-Pro.local>
2018-01-07 21:21:46 +0700

    Part1-Lesson9

--- users-service/project/config.py
+++ users-service/project/config.py
@@ -12,14 +12,17 @@ class DevelopmentConfig(BaseConfig):
     """Development Configuration"""
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = "my_precious"
 
 class TestingConfig(BaseConfig):
     """Testing Configuration"""
     DEBUG = True
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+    SECRET_KEY = "my_precious"
 
 class ProductionConfig(BaseConfig):
     """Production Configuration"""
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = "my_precious"
