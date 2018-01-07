e886f73480c9842cd651dc6ae4834da362101a7f
Yusak Setiawan <zivlab@Yusaks-MacBook-Pro.local>
2018-01-07 18:33:20 +0700

    Part1-Lesson6

--- users-service/project/__init__.py
+++ users-service/project/__init__.py
@@ -1,15 +1,29 @@
 # users-service/project/__init__.property
 
+import os
+import datetime
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
-import sys
+db = SQLAlchemy(app)
 
-print(app.config, file=sys.stderr)
+# model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
--- users-service/project/config.py
+++ users-service/project/config.py
@@ -1,19 +1,25 @@
 # users-service/project/config.py
 
+import os
+
 class BaseConfig:
     """Base Configuration"""
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(BaseConfig):
     """Development Configuration"""
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(BaseConfig):
     """Testing Configuration"""
     DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig(BaseConfig):
     """Production Configuration"""
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
