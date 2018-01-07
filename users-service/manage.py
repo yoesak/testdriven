3119c0f09ceaba7cd9423927bbaed038530b98a5
Yusak Setiawan <zivlab@Yusaks-MacBook-Pro.local>
2018-01-07 21:00:12 +0700

    Part1-Lesson8

--- users-service/manage.py
+++ users-service/manage.py
@@ -1,27 +1,33 @@
 # users-service/manage.py
 
-from flask_script import Manager
-from project import app, db
+
 import unittest
 
+from flask_script import Manager
 
+from project import create_app, db
+from project.api.models import User
+
+
+app = create_app()
 manager = Manager(app)
 
 @manager.command
-def recreate_db():
-    """Recreates a database"""
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
-
-@manager.command
 def test():
-    """Runs the tests without code coverage."""
+    """Runs the unit tests without test coverage."""
     tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         return 0
     return 1
+
+@manager.command
+def recreate_db():
+    """Recreates a database."""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 
 if __name__ == '__main__':
     manager.run()
